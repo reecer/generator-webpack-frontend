937b7514476de20899df5726d26722535550eb4c
Reece Robinson <reecekrobinson@gmail.com>
2014-10-09 16:07:55 -0600

    Change from static/ to src/ directory structure. Because it's not really static!

--- app/index.js
+++ app/index.js
@@ -6,13 +6,13 @@ var yeoman = require('yeoman-generator');
 var WebpackFrontend = yeoman.generators.Base.extend({
   writing: {
     app: function () {
-      this.dest.mkdir('static');
-      this.dest.mkdir('static/components');
-      this.dest.mkdir('static/stylesheets');
+      this.dest.mkdir('src');
+      this.dest.mkdir('src/components');
+      this.dest.mkdir('src/stylesheets');
 
-      this.src.copy('_app.jsx'          , 'static/components/app.jsx');
-      this.src.copy('_app.scss'         , 'static/stylesheets/app.scss');
-      this.src.copy("_index.html"       , "static/index.html");
+      this.src.copy('_app.jsx'          , 'src/components/app.jsx');
+      this.src.copy('_app.scss'         , 'src/stylesheets/app.scss');
+      this.src.copy("_index.html"       , "src/index.html");
       this.src.copy('_bower.json'       , 'bower.json');
       this.src.copy('_package.json'     , 'package.json');
       this.src.copy('_webpack.config.js', 'webpack.config.js');      
